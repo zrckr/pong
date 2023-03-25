7f6ee46fa2aa17f4912711a8c66ba0e60a4d8fdf
zerocker <zerocloude@gmail.com>
2023-03-25 22:52:19 +0900

    feat: implement goal logic

--- goal/goal.gd
+++ goal/goal.gd
@@ -1,9 +1,17 @@
 extends Area2D
 
+@export_enum("Player One", "Player Two")
+var score_for_player: int
+
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 
-func _on_body_entered(_body: PhysicsBody2D) -> void:
-	pass
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	if body is Ball:
+		_reset_ball_and_score(body)
+
+
+func _reset_ball_and_score(ball: Ball) -> void:
+	ball.reset_speed_and_position()
